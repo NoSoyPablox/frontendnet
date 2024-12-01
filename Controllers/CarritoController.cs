7cfbf2482bd232225fdc38babb81270dcd9b6e23
NoSoyPablox <pablohernan298@gmail.com>
2024-12-01 14:31:26 -0600

    Cart controller and views updated

--- Controllers/CarritoController.cs
+++ Controllers/CarritoController.cs
@@ -8,10 +8,109 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 namespace frontendnet;
 
 [Authorize(Roles = "Usuario")]
-public class CarritoController() : Controller
+public class CarritoController(CarritosClientService carritos, IConfiguration configuration) : Controller
 {
+
     public IActionResult Index()
     {
         return View();
     }
+
+    public async Task<IActionResult> CarritoComprasPartial()
+    {
+        List<Carrito>? lista = [];
+        List<CarritoProducto> listaproductos = [];
+        ViewBag.Url = configuration["UrlWebAPI"];
+        try
+        {
+            lista = await carritos.GetAsync();
+        }
+        catch (HttpRequestException ex)
+        {
+            if (ex.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                return RedirectToAction("Salir", "Auth");
+            }
+        }
+
+        if (lista != null && lista.Count > 0)
+        {
+            foreach (var item in lista)
+            {
+                if (item.CarritoProductos != null)
+                {
+                    listaproductos = item.CarritoProductos;
+                }
+            }
+        }
+        return PartialView("_PartialCarrito", listaproductos);
+    }
+
+    public async Task<IActionResult> Eliminar(int itemid, bool? showError = false)
+    {
+        List<CarritoProducto>? itemToDelete = null;
+        CarritoProducto? itemCarrito = null;
+        try
+        {
+            itemToDelete = await carritos.GetProductoCarritoAsync(itemid);
+            if (itemToDelete == null || itemToDelete.Count == 0)
+            {
+                return NotFound();
+            }
+            else{
+                itemCarrito = itemToDelete[0];
+            }
+            if (showError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] = "No ha sido posible realizar la acción. Inténtelo nuevamente.";
+            }
+        }
+        catch (HttpRequestException ex)
+        {
+            if (ex.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                return RedirectToAction("Salir", "Auth");
+            }
+        }
+        return View(itemCarrito);
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> DeleteProducto(int id)
+    {
+        try
+        {
+            await carritos.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+        catch (HttpRequestException ex)
+        {
+            if (ex.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                return RedirectToAction("Salir", "Auth");
+            }
+        }
+        return RedirectToAction(nameof(Eliminar), new { itemid = id, showerror = true });
+    }
+
+    [HttpPut]
+public async Task<IActionResult> Producto(int id, int cantidad)
+{
+    try
+    {
+        await carritos.PutAsync(id, cantidad);
+        return Json(new { success = true });
+    }
+    catch (HttpRequestException ex)
+    {
+        if (ex.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+        {
+            return Json(new { success = false, redirectUrl = Url.Action("Salir", "Auth") });
+        }
+    }
+    return BadRequest();
+}
+
+
 }
