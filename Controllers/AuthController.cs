aa1f315f6d4e94c5e8d55e62df6969f43f50f83c
NoSoyPablox <pablohernan298@gmail.com>
2024-12-03 12:58:24 -0600

    Fix for total price and register view and auth controller methods

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -5,10 +5,11 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace frontendnet;
 
-public class AuthController(AuthClientService auth) : Controller{
+public class AuthController(AuthClientService auth, RolesClientService roles, UsuariosClientService usuarios) : Controller{
     [AllowAnonymous]
     public IActionResult Index()
     {
@@ -57,4 +58,49 @@ public class AuthController(AuthClientService auth) : Controller{
         //Sino redirige a la pagina inicial
         return RedirectToAction("Index", "Auth");
     }
+
+    public async Task<IActionResult> Crear()
+    {
+        await RolesDropDownListAsync();
+        return View();
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> CrearAsync(UsuarioPwd itemToCreate)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                await usuarios.PostAsync(itemToCreate);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("Email", "No ha sido posible crear la cuenta. Intentelo nuevamente.");
+            }
+        }
+        return View(itemToCreate);
+
+    }
+
+    private async Task RolesDropDownListAsync(object? rolSelecionado = null)
+    {
+        var listado = await roles.GetAsync();
+
+        // Filtrar solo el rol con el nombre "Usuario"
+        if (listado == null) return;
+        var usuarioRol = listado.FirstOrDefault(rol => rol.Nombre == "Usuario");
+
+        // Crear la lista para el DropDownList
+        if (usuarioRol != null)
+        {
+            ViewBag.Rol = new SelectList(new[] { usuarioRol }, "Nombre", "Nombre", rolSelecionado);
+        }
+        else
+        {
+            ViewBag.Rol = new SelectList(Enumerable.Empty<object>(), "Nombre", "Nombre");
+        }
+}
 }
